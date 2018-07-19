8ddf0140a04c7ffe4f2bc5525e29c4105d444c7d
StevenGrl <steven.grillon.45@gmail.com>
2018-07-19 12:04:28 +0200

    suppression du console log

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     let imageFile = $('#ad_imageFile')
     $(imageFile).on('input', function() {
-        console.log(this)
         let imageFile = $('#ad_imageFile').val().split('\\').pop()
         $('#ad_imageFile').next().text(imageFile)
     });
